5e80528ec05520acb2c53809c3052957cec1a5db
Antero <anteroavalos2508@gmail.com>
2022-07-01 20:05:23 -0700

    Calculate population and sample mean

---
+++ populationMean.js
@@ -0,0 +1,23 @@
+let population = [74,87,71]
+
+function popMean(arr){
+    let sum = arr.reduce((a, b) => a + b, 0)
+    
+    return sum/arr.length;
+}
+console.log('Population length: ' + population.length)
+console.log('Sum of data set: ' + population.reduce((a, b) => a + b, 0))
+console.log('Population Mean:  μ = ' + popMean(population))
+console.log('Sample Mean:  X̄ = ' + popMean(population).toFixed(1))
+
+function findMissingItem(arr, numberOfItems, median){
+    let sum = arr.reduce((a, b) => a + b, 0)
+    console.log(sum, 29.5*6)
+    return (median * numberOfItems) - sum
+}
+
+let missingItemArray = [7, 12, 27, 41, 49]
+console.log(findMissingItem(missingItemArray, 6, 29.5))
+
+let bottled = [5.15, 5.09, 5.37, 5.20, 4.90, 5.23, 5.52, 5.37, 5.13, 5.37, 5.21, 5.24]
+let tap = [7.64, 7.45, 7.46, 7.50, 7.68, 7.69, 7.45, 2.72, 7.56, 7.46, 7.52, 7.46]
