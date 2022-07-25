2ad4de675ae1d6eef6864b47dbfc23c9023a262d
Antero <anteroavalos2508@gmail.com>
2022-07-24 22:31:23 -0700

    ...

--- Other/relativeFreq.js
+++ Other/relativeFreq.js
@@ -6,7 +6,7 @@ function sumFrequencies(arr){
     return arr.reduce((a, b) => a + b, 0)
 }
 
-let observations = [13,11,0,1]
+let observations = [379, 378, 983, 322, 84]
 
 
 let sum = sumFrequencies(observations)
@@ -23,3 +23,18 @@ relFreq(sum, observations, resArr)
 
 console.log('Relative Freq. Distribution:\n')
 console.log(resArr)
+
+
+
+9,24
+10.9,10
+18.7,104
+17.1,16
+14.7,8.5
+12.2,21
+11.1,-6.6
+9.9,27
+9,134
+11.1,-10
+14.1,31
+13.8,25
